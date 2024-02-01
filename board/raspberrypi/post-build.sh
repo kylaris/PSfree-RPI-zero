fc63f356dacbea22b361b8bf47455a9f11274cdb
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2024-02-01 11:52:40 +0000

    add ps4 exploit hosting

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,18 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+rm -f ${TARGET_DIR}/etc/init.d/S80dnsmasq
+rm -f ${TARGET_DIR}/etc/init.d/S90hostapd
+chmod 755 ${TARGET_DIR}/etc/sudoers
+cp board/raspberrypi/Executables/sudoers ${TARGET_DIR}/etc/sudoers
+cp board/raspberrypi/Executables/nginx.conf ${TARGET_DIR}/etc/nginx/nginx.conf
+cp -r board/raspberrypi/PiZeroProject-Final/. ${TARGET_DIR}/usr/html
+cp board/raspberrypi/Executables/hostapd.conf ${TARGET_DIR}/etc/hostapd.conf
+cp board/raspberrypi/Executables/interfaces ${TARGET_DIR}/etc/network/interfaces
+cp board/raspberrypi/Executables/S02modules ${TARGET_DIR}/etc/init.d/S02modules
+cp board/raspberrypi/Executables/IpDNSupd ${TARGET_DIR}/etc/network/if-up.d/IpDNSupd
+chmod ugo+rwx ${TARGET_DIR}/usr/html
+
+mkdir -p ${TARGET_DIR}/etc/customscript
+cp -r board/raspberrypi/Executables/customscript/. ${TARGET_DIR}/etc/customscript
