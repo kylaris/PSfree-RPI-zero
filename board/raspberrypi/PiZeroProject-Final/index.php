8f8ecf1d04d28ae298d8f9b79f04da85c3ea5d9d
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2024-02-01 13:15:24 +0000

    user newer webpage

--- board/raspberrypi/PiZeroProject-Final/index.php
+++ board/raspberrypi/PiZeroProject-Final/index.php
@@ -17,7 +17,41 @@
 		  background-color: #007bff;
 		  color: White;
 		}
+        
+        .buttonOrange {
+		  background-color: #ff9800;  
+		  border-radius: 5px;
+		  color: white;
+		  padding: .5em;
+		  text-decoration: none;
+		  height:100%;
+		  display:inline-table;
+		  font-family: system-ui;
+		}
 
+		.buttonOrange:focus,
+		.buttonOrange:hover {
+		  background-color: #007bff;
+		  color: White;
+		}
+
+        .buttonlgreen {
+		  background-color: #0c635f;  
+		  border-radius: 5px;
+		  color: white;
+		  padding: .5em;
+		  text-decoration: none;
+		  height:100%;
+		  display:inline-table;
+		  font-family: system-ui;
+		}
+
+		.buttonlgreen:focus,
+		.buttonlgreen:hover {
+		  background-color: #007bff;
+		  color: White;
+		}
+		
 		.titlehead {
 		  background-color: #003263;  
 		  border-radius: 5px;
@@ -37,94 +71,293 @@
 		}
 
 		.bg {
-		  /* The image used */
-		  background-image: url("ps4.jpg");
-
-		  
-
-		  /* Center and scale the image nicely */
+		  background-color: black;
 		  background-position: center;
-		  background-repeat: no-repeat;
 		  background-size: cover;
 		}
+		.aGit {
+            text-decoration: none;
+        }
+        .aGit:link, .aGit:visited {
+            color: white;
+        }
+        .aGit:hover {
+            color: blue;
+        }
 	</style>
 	<head>
 		<title>PS4 Jailbreak 9.00</title>
 		<script>
+            
+            var exec_type = "payload";
+            var payloadData = "";
+            function loadGoldHenNew(PLfile)
+            {
+                    progress.innerHTML="Loading Exploit + "+PLfile+" Please Wait !!";
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', PLfile, true);
+                    xhr.overrideMimeType('text/plain; charset=x-user-defined');
+                    xhr.onload = function(e) {
+                    if (this.status == 200) {
+                        payloadData = this.response;
+                        setTimeout(poc, 1500);
+                    }else
+                    {
+                        alert("Failed to load " + PLfile + " - " + this.status);
+                return;
+                    }};
+                    xhr.send();
+            }
+
 			function loadcomplete() {
 				document.getElementById("progress").innerHTML=sessionStorage.Queue+" Loaded Successfully !!";
 				sessionStorage.Queue="";
+				sessionStorage.clear();
+				localStorage.clear();
+				var payloadData = "";
+			}
+            
+            function sendPayload(pfile){
+                if(pfile.substring(pfile.length-3)!=='bin'){
+                    alert(pfile+ " Not a bin file");
+                    return;
+                }
+                progress.innerHTML="Loading "+pfile+".. Please Wait !!";
+    			var hr = new XMLHttpRequest();
+    			const url = "script.php";
+    			const vars = "value=Payload"+"&pfile="+encodeURIComponent(pfile);
+    			hr.open("POST", url, true);
+    			hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    			hr.onreadystatechange = function() {
+	    			if(hr.readyState == 4 && hr.status == 200) {
+		    			var return_data = hr.responseText;
+		    			progress.innerHTML=return_data;
+		    			alert(return_data);
+	    			}
+    			}
+    			hr.send(vars);
 			}
 			
-    </script>
-    <script src="./int64.js"></script>
-    <script src="./rop.js"></script>
-    <script src="./kexploit.js"></script>
-    <script type=module src="./alert.mjs"></script>
+			function FanControll(){
+                var fanValue=document.getElementById('fanTemp').value;
+                var pfile="Resources/FanControll/fan"+fanValue+".bin";
+                progress.innerHTML="Loading "+pfile+".. Please Wait !!";
+    			var hr = new XMLHttpRequest();
+    			
+    			const url = "script.php";
+    			const vars = "value=FanControll"+"&pfile="+encodeURIComponent(pfile);
+    			hr.open("POST", url, true);
+    			hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    			hr.onreadystatechange = function() {
+	    			if(hr.readyState == 4 && hr.status == 200) {
+		    			var return_data = hr.responseText;
+		    			progress.innerHTML=return_data;
+		    			alert(return_data);
+	    			}
+    			}
+    			hr.send(vars);
+			}
+			
+			function resourceUpdate(opType,opValue){
+                progress.innerHTML="Updating Please Wait !!";
+    			var hr = new XMLHttpRequest();
+    			const url = "script.php";
+    			const vars = "value=resourceUpdate"+"&OpType="+encodeURIComponent(opType)+"&OpValue="+encodeURIComponent(opValue);
+    			hr.open("POST", url, true);
+    			hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    			hr.onreadystatechange = function() {
+	    			if(hr.readyState == 4 && hr.status == 200) {
+		    			var return_data = hr.responseText;
+		    			progress.innerHTML=return_data;
+		    			alert(return_data);
+	    			}
+    			}
+    			hr.send(vars);
+    			
+			}
+			
+			function AutoJB(val){
+                if(val==="StartJBSequence" ){
+                   loadGoldHenNew("<?php echo exec("ls  | grep -o 'g.*.bin' | head -n1") ?>");
+                    return;
+               }
+    			var hr = new XMLHttpRequest();
+    			const url = "script.php";
+    			const vars = "value=AutoJb"+"&opType="+encodeURIComponent(val);
+    			hr.open("POST", url, true);
+    			
+    			hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    			hr.onreadystatechange = function() {
+	    			if(hr.readyState == 4 && hr.status == 200) {
+		    			var return_data = hr.responseText.trim();
+		    			if (return_data==="StartJBSequence"){
+                          loadGoldHenNew("<?php echo exec("ls  | grep -o 'g.*.bin' | head -n1") ?>");
+		    			}
+	    			}
+    			}
+    			hr.send(vars);
+			}
+			</script>
+    <script src="int64.js"></script>
+    <script src="rop.js"></script>
+    <script src="kexploit.js"></script>
+    <script type=module src="alert.mjs"></script>
 	</head>
 	<body class="bg">
 		<script>
-			window.onload = function(e){if(sessionStorage.Queue==null){sessionStorage.Queue="";}else if(sessionStorage.Queue!=""){executependingqueue(sessionStorage.Queue);}};
-			window.applicationCache.ondownloading=function(){document.getElementById("progress").innerHTML="Page Caching Started!!"};
-			window.applicationCache.onprogress=function(a){document.getElementById("progress").innerHTML="Caching Status: "+(Math.round(100*(a.loaded/a.total)))+"% Completed"};
-			window.applicationCache.oncached=function(){document.getElementById("progress").innerHTML="Page Cached Successfully!!";setTimeout(function(){document.getElementById("progress").innerHTML="Turn Off the Internet & Reopen the Page to Run the Exploit!!"; }, 1500);};
+			window.onload = function(e){if(sessionStorage.Queue==null){sessionStorage.Queue="";}else if(sessionStorage.Queue!=""){executependingqueue(sessionStorage.Queue);}AutoJB('PageLoad');};
 		</script>
-		<?php $ipadd = shell_exec("/sbin/ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");?>
-		<h1 id="progress" class="titlehead">PS4 Jailbreak 9.00 - <?php echo $ipadd ?></h1><hr><br>
+		<h1 id="progress" class="titlehead">PS4 Jailbreak 9.00 - <?php echo $_SERVER['SERVER_ADDR'] ?></h1><hr><br>
 		<input type="hidden" id="psip" value="localhost"/>
 		<div>
-			<table align="center" style="width:600px;margin-top:30px;">
+			<table align="center" style="width:1000px;margin-top:30px;">
 			<tr>
 			<td align="center" id="exploit">
-			<a href="#" class="button" onclick="goldhen(); return false" style="width:60%">Exploit + GoldHEN</a>
-			</td>
-			<td >
-			<a href="RaspberyPi.php" class="button" style="width:40%">RaspberryPi</a>
-			</td>
-			</tr>
-			<tr><td><br/></td></tr>
-			<tr>
-			<td colspan="2" align="center"><hr><br></td>
-			</tr>
-			<tr>
-			<td align="center" colspan="2"><a href="#" class="button" onclick="app2usb(); return false" style="width:43%">App2USB</a>
-			<a href="#" class="button" onclick="appdumper(); return false" style="width:43%">App Dumper</a></td>
-			</tr><tr><td><br/></td></tr>
-			<tr>
-			<td align="center" colspan="2"><a href="#" class="button" onclick="disableupdates();return false" style="width:43%">Disable Updates</a>
-			<a href="#" class="button" onclick="enableupdates(); return false" style="width:43%">Enable Updates</a></td>
-			</tr><tr><td><br/></td></tr>
-			<tr>
-			<tr>
-			<td align="center" colspan="2">
-			<a href="#" class="button" onclick="ftp(); return false" style="width:43%">FTP</a>
-			<a href="#" class="button" onclick="fanthreshold(temp.value); return false" style="width:43%;border-radius: 5px 0px 0px 5px;">Set Fan Threshold</a>
-			<select id="temp" style="height:42px;border-radius: 0px 5px 5px 0px;position: absolute;" class="button">
-			<option value="40">40°C</option><option value="41">41°C</option><option value="42">42°C</option><option value="43">43°C</option><option value="44">44°C</option><option value="45">45°C</option><option value="46">46°C</option><option value="47">47°C</option><option value="48">48°C</option><option value="49">49°C</option><option value="50">50°C</option><option value="51">51°C</option><option value="52">52°C</option><option value="53">53°C</option><option value="54">54°C</option><option value="55">55°C</option><option value="56">56°C</option><option value="57">57°C</option><option value="58">58°C</option><option value="59">59°C</option><option value="60">60°C</option><option value="61">61°C</option><option value="62">62°C</option><option value="63">63°C</option><option value="64">64°C</option><option value="65">65°C</option><option value="66">66°C</option><option value="67">67°C</option><option value="68">68°C</option><option value="69">69°C</option><option value="70" selected>70°C</option><option value="71">71°C</option><option value="72">72°C</option><option value="73">73°C</option><option value="74">74°C</option><option value="75">75°C</option><option value="76">76°C</option><option value="77">77°C</option><option value="78">78°C</option><option value="79">79°C</option>
+			<a href="#" class="button" onclick="loadGoldHenNew('<?php echo exec("ls  | grep -o 'g.*.bin' | head -n1") ?>'); return false" id="henButton">GoldHEN</a>
+			<a href="RaspberyPi.php" class="button">RaspberryPi</a>
+			<a href="#" class="button" onclick="FanControll(); return false">Set Fan Threshold</a>
+			<select id="fanTemp" style="height:42px;border-radius: 0px 5px 5px 0px;position: absolute;" class="button">
+			<option value="50">50°C</option><option value="55">55°C</option><option value="60">60°C</option><option value="65">65°C</option><option value="70">70°C</option><option value="75">75°C</option><option value="80">80°C</option>
 			</select>
-			</td>
-			</tr><tr><td><br/></td></tr>
-			<tr>
-			<td align="center" colspan="3"><a href="#" class="button" onclick="backup(); return false" style="width:29%">DB BackUp</a>
-			<a href="#" class="button" onclick="restore(); return false" style="width:29%">DB Restore</a>
-			<a href="#" class="button" onclick="rifrenamer(); return false" style="width:29%">RIF Renamer</a></td>
-			</tr><tr><td><br/></td></tr>
-			<tr>
-			<td align="center" colspan="3"><a href="#" class="button" onclick="moduledumper(); return false" style="width:29%">Module Dumper</a>
-			<a href="#" class="button" onclick="kerneldumper(); return false" style="width:29%">Kernel Dumper</a>
-			<a href="#" class="button" onclick="kernelclock(); return false" style="width:29%">Kernel Clock</a></td>
-			</tr><tr><td><br/></td></tr>
-			<tr>
-			<td align="center" colspan="3"><a href="#" class="button" onclick="ps4debug(); return false" style="width:29%">PS4 Debug</a>
-			<a href="#" class="button" onclick="webrte(); return false" style="width:29%">WebRTE</a>
-			<a href="#" class="button" onclick="permanentuart(); return false" style="width:29%">Permanent UART</a></td>
-			</tr><tr><td><br/></td></tr>
-			<tr>
-			<td align="center" colspan="3"><a href="#" class="button" onclick="exitidu(); return false" style="width:29%">Exit IDU</a>
-			<a href="#" class="button" onclick="todex(); return false" style="width:29%">To-DEX</a>
-			<a href="#" class="button" onclick="disableaslr(); return false" style="width:29%">Disable ASLR</a></td>
-			</tr>
 			
-			</table></div>
+			</td>
+			</tr>
+			</table>
+			
+			<div id=mainMenu style="text-align:center;display:display">
+			<table align="center" style="width:1000px;margin-top:30px;">
+			<tr>
+			
+			<?php
+                $mainModFunction='<script> function mainMenumods(){ mainMenu.style.display = "block"; BackToMainMenu.style.display = "none"; mainMenuBottom.style.display = "block";';
+                $SubFolderArray = array();
+                $scan = scandir('Bin');
+                foreach($scan as $fldr) {
+                
+                    if (is_dir("Bin/$fldr") && ($fldr != ".")  && ($fldr != "..") && ($fldr != "JsBins")) {
+                            $binlist= glob("Bin/".$fldr."/*.bin");
+                            $Imglist= glob("Bin/".$fldr."/*.img");
+                            if (sizeof($binlist) !=0 || sizeof($Imglist)!=0) {
+                                $mainModFunction = $mainModFunction . $fldr.'.style.display = "none";';
+                                echo '<script> function '.$fldr.'mods(){ mainMenu.style.display = "none"; mainMenuBottom.style.display = "none"; '.$fldr.'.style.display = "block"; BackToMainMenu.style.display = "block"; }</script>';
+                                array_push($SubFolderArray,$fldr);
+                            }
+
+                    }
+                }
+                $mainModFunction= $mainModFunction .' }</script>';
+                echo $mainModFunction;
+                
+                
+                function cleanName($ActName) {
+
+                        $NewName = substr($ActName, 0, -4);
+                        $FinalName="";
+                        $Arry=explode("-",$NewName);
+                        foreach($Arry as $wd) {
+                            $FinalName.=ucwords($wd);
+                        }
+                    return $FinalName;
+                }
+                
+                function createButtons($BaseFolder,$SubFolders){
+                    $path=$BaseFolder.'/*.bin';
+                    $filenames = array_merge(glob($BaseFolder.'/*.bin'),glob($BaseFolder.'/*.img'));
+                    natcasesort($filenames);                   
+                    $NumOFelementinRow=6;
+                    if (sizeof($filenames) != 0){
+                        $NumOFelementinRow=  intdiv(sizeof($filenames)+sizeof($SubFolders), 6)+1;  
+                    }
+                    $Counter=0;
+                    foreach ($filenames as $filename) {
+                        $Clean=cleanName(end(explode("/",$filename)));
+                        if ( substr_count(strtolower($Clean), 'fan')==0 ){
+                            if(substr($BaseFolder, -7) == 'GoldHen'){
+                                echo '<td  align="center">';
+                                echo '<a href="#" class="button" onclick="resourceUpdate('."'GoldHenUpdate','".$filename."'". '); return false" style="width:150px">'.$Clean.'</a>';
+                                echo '</td>';
+                            } else if(substr($BaseFolder, -6) == 'USBimg'){
+                                echo '<td  align="center">';
+                                echo '<a href="#" class="button" onclick="resourceUpdate('."'USBimgUpdate','".$filename."'". '); return false" style="width:150px">'.$Clean.'</a>';
+                                echo '</td>';
+                            }
+                            else{
+                                echo '<td  align="center">';
+                                echo '<a href="#" class="button" onclick="sendPayload('."'".substr($filename,4)."'". '); return false" style="width:150px">'.$Clean.'</a>';
+                                echo '</td>';
+                            }
+                            $Counter+=1;
+                        }
+                        
+                        
+                        if ($Counter==$NumOFelementinRow){
+                            $Counter=0;
+                            echo "</tr><tr></tr><tr></tr><tr align='center'>";
+                        }
+                        
+                    }
+                    
+                    foreach($SubFolders as $fldr) {
+                            echo '<td  align="center">';
+                            echo '<a href="#" class="buttonlgreen" onclick="'.$fldr.'mods(); return false" style="width:150px">'.$fldr.'</a>';
+                            echo '</td>';
+                            $Counter+=1;
+                            if ($Counter==$NumOFelementinRow){
+                                $Counter=0;
+                                echo "</tr><tr></tr><tr></tr><tr align='center'>";
+                            }
+                    }
+                }
+                createButtons('Bin',$SubFolderArray);   
+
+			?>
+			</tr>
+			</table>
+            <br><br>
+			</div>
+			<?php
+                foreach($SubFolderArray as $fldr) {
+                    if (is_dir("Bin/$fldr") && ($fldr != ".")  && ($fldr != "..")) {
+                        echo '<div id='.$fldr.' style="text-align:center;display:none">';
+                        echo '<table align="center" style="width:1000px;margin-top:30px;">';
+                        echo '<tr>';
+                        createButtons('Bin/'.$fldr,[]);
+                        echo '</tr>';
+                        echo '</table>';
+                        echo '</div>';
+                    }
+                }
+                
+			?>
+			<div id=mainMenuBottom style="text-align:center;display:display">
+			<table  align="center">
+			<tr>
+			<td align="center" style="color:white;" >
+                <a href="Resources/OffAct/index.html" class="button" >Web Activator</a>&nbsp;&nbsp;
+                <a href="Resources/OffTrainer/index.html" class="button">Offline Trainer</a>&nbsp;&nbsp;
+                <a href="Resources/virtualKB/index.html" class="button">Virtual Keyboard</a>&nbsp;&nbsp;
+                <a href="Resources/jsLoader/index.php" class="button">Other Bins</a>
+            </td>
+			</tr>
+			</table>
+			</div>
+			<div id=BackToMainMenu style="text-align:center;display:none">
+			<table  align="center">
+			<tr>
+			<td align="center"><a href="#" class="button" onclick="mainMenumods(); return false" style="width:200px">Back to Main Menu</a></td>
+			</tr>
+			</table>
+			</div>
+			</table>
+			<table  align="center" style="width:1000px;margin-top:30px;">
+			<tr>
+			<td align="center"><font  style="color:white;font-size: 20px;">Embeded OS, Web Host, Emulation designed by <a class="aGit" href="https://github.com/PaulJenkin"><I><b><u>PaulJenkin</u></b></I></a></font>
+			<br><br>
+			<font  style="color:white;">Credits to <a class="aGit" href="https://github.com/d3structor84"><I><b><u>d3structor84</u></b></I></a> for app design</font>
+			</td>
+			</tr>
+			</table>
+			</div>
+			</script>
 	</body>
 </html>
